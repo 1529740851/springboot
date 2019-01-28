860922ec1f53a486081f026146b7c0ebc7f72914
Administrator <Administrator@PC>
2019-01-28 09:47:43 +0800

    添加异常捕获类

---
+++ src/main/java/com/example/demo/Controller/ExceptionController.java
@@ -0,0 +1,28 @@
+package com.example.demo.Controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+@ResponseBody
+public class ExceptionController {
+
+	private static Logger logger = LoggerFactory.getLogger(ExceptionController.class);
+
+	@ExceptionHandler(Exception.class)
+	@ResponseBody
+	public Map<String, Object> exceptionHandler() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("errorCode", "101");
+		map.put("errorMsg", "系統错误!");
+		return map;
+
+	}
+
+}
