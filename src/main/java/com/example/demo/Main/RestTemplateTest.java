a8282f59efcae1ba0620c4d91ac0f921d59a8577
Administrator <Administrator@PC>
2019-02-19 17:07:47 +0800

    RestTemplate添加post请求方法

--- src/main/java/com/example/demo/Main/RestTemplateTest.java
+++ src/main/java/com/example/demo/Main/RestTemplateTest.java
@@ -1,6 +1,8 @@
 package com.example.demo.Main;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import com.alibaba.fastjson.JSON;
@@ -12,6 +14,9 @@ public class RestTemplateTest {
 		 
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity responseEntity = restTemplate.getForEntity(url, String.class);
+       
+ //       MultiValueMap<String, String> paramMap = new LinkedMultiValueMap<>();
+  //        restTemplate.postForObject(url, paramMap, String.class);
 //        System.out.print(responseEntity.toString());
 //        System.out.println();
         System.out.print(JSON.toJSONString(responseEntity.getBody())); //responseEntity.getBody()));
