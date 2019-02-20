a7e833ff68cf8db1f3086d5730ee468ecd1f3ea1
Administrator <Administrator@PC>
2019-02-20 09:45:22 +0800

    添加生成订单号

--- src/main/java/com/example/demo/Main/RedisTest.java
+++ src/main/java/com/example/demo/Main/RedisTest.java
@@ -1,12 +1,15 @@
 package com.example.demo.Main;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.example.demo.Util.RedisConfig;
+
 import com.example.demo.Util.RedisUtil;
 
 @RunWith(SpringRunner.class)
@@ -17,12 +20,20 @@ public class RedisTest {
 	private RedisUtil redisUtil;
 	
 	@Test
-    public void test() throws Exception {
-		
-	
+    public void test() throws Exception {		
 		System.out.println("xxx");
 		redisUtil.set("xx", "11");
 		System.out.println(redisUtil.get("xx"));
+	}
+	
+	@Test
+	public void indexTest(){
+		String name="reg";
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+        String format = simpleDateFormat.format(new Date());
+        String key = name+format;
+        Long num = redisUtil.incr(key,1L);
+        System.out.println(num);
 	}
 
 }
