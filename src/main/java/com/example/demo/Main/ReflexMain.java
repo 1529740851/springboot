bc40cdb08905af221f25091d3e510e93903acca7
Administrator <Administrator@jiang_longfu>
2019-03-18 14:37:37 +0800

    11

--- src/main/java/com/example/demo/Main/ReflexMain.java
+++ src/main/java/com/example/demo/Main/ReflexMain.java
@@ -6,41 +6,47 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
-import com.example.demo.Annotation.Excel;
+import com.example.demo.Annotation.Exceld;
 import com.example.demo.entity.Student;
 
 public class ReflexMain {
 	
 	public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException {
-		Student stu=new Student(1,"12",33);
-		Student stu1=new Student(2,"13",22);
-		Student stu2=new Student(3,"14",33);
-		Student stu3=new Student(4,"15",24);
+		Student stu=new Student(1,"张三",33);
+		Student stu1=new Student(2,"李四",22);
+		Student stu2=new Student(3,"王五",33);
+		Student stu3=new Student(4,"孙六",24);
 		List<Student> stus=new ArrayList<Student>();
 		stus.add(stu);
 		stus.add(stu1);
 		stus.add(stu2);
 		stus.add(stu3);
-		getField(stus);
+		getField(stus,Student.class);
 	}
 	
-	public static void getField(List<?> list) throws IllegalArgumentException, IllegalAccessException{
+	public static void getField(List<?> list,Class classs ) throws IllegalArgumentException, IllegalAccessException{
 		Class<?> entityClass = list.get(0).getClass();
 		entityClass.getAnnotations();
+		for(Field field : classs.getDeclaredFields()){
+			Exceld e=field.getAnnotation(Exceld.class);
+			System.out.println(e==null?"":e.name());
+		}
 		Iterator<?> iter = list.iterator();
 		while (iter.hasNext()) {
 			Object obj=iter.next();
 			for (Field field : obj.getClass().getDeclaredFields()) {
 				field.setAccessible(true);
-				Object Value = field.get(obj);
-				Excel e= field.getAnnotation(Excel.class);
-				System.out.println(e==null?"":e.name());
+				Exceld e=field.getAnnotation(Exceld.class);
+				if(e==null){
+					continue;
+				}
+				Object Value = field.get(obj);				
 				if(Value !=null){
 					if(field.getType()==String.class){
-						System.out.println("String"+(String)Value);
+						System.out.println("String"+Value);
 					}
 					if(field.getType()==Integer.class){
-						System.out.println("Integer"+(Integer)Value);
+						System.out.println("Integer"+Value);
 					}
 					if(field.getType()==Date.class){
 						System.out.println("String"+(Date)Value);
