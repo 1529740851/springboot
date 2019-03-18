e3801b1f550084db91b74ba331ce6f356c634fc4
Administrator <Administrator@jiang_longfu>
2019-03-18 14:38:36 +0800

    11

--- src/main/java/com/example/demo/Annotation/Exceld.java
+++ src/main/java/com/example/demo/Annotation/Exceld.java
@@ -7,6 +7,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
-public @interface Excel {
+public @interface Exceld {
 	public String name() default "";
 }
