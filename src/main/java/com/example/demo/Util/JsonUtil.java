b409789fc93493d9e99cff8a924c3e8c9b9663bd
1529740851 <13789094231@163.com>
2019-07-08 15:33:57 +0800

    11

--- src/main/java/com/example/demo/Util/JsonUtil.java
+++ src/main/java/com/example/demo/Util/JsonUtil.java
@@ -4,12 +4,18 @@ import java.util.List;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.annotation.JSONField;
+import org.apache.poi.ss.formula.functions.T;
 
 
 public class JsonUtil {
-	
-	//@JSONField(format="yyyy-MM-dd")Ê¹ÓÃ
+
+	//@JSONField(format="yyyy-MM-dd")ä½¿ç”¨
 	public static List<?> toList(String s,Class zlass){
 		return JSON.parseArray(s, zlass);
+	}
+
+
+	public  static  String toString(T t){
+		return  JSON.toJSONString(t);
 	}
 }
